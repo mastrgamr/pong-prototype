07653b454b7f3f1e4f355bd9848d11575038bb21
Stuart <mastrgamr@users.noreply.github.com>
2010-11-18 03:19:45 +0000

    initial commit

---
+++ PongPrototype/PongPrototype/PongPrototype/AI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PongPrototype
+{
+    abstract class AI
+    {
+        protected Vector2 position;
+        public Texture2D texture;
+        public bool isCPU;
+        public string soundFile {get; private set;}
+
+        public AI(Texture2D texture, Vector2 position, string soundFile)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.soundFile = soundFile;
+        }
+
+        public AI(Texture2D texture, Vector2 position, bool isCPU, string soundFile)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.isCPU = isCPU;
+            this.soundFile = soundFile;
+        }
+
+        public void followBall(Rectangle clientBounds, Paddle player, Paddle enemy, Ball ball)
+        {
+                if (ball.position.Y > enemy.position.Y + 40 && ball.position.X > clientBounds.Width / 2)
+                {
+                    enemy.position.Y += 7.0f;
+                }
+
+                if (ball.position.Y < enemy.position.Y + 40 && ball.position.X > clientBounds.Width / 2)
+                {
+                    enemy.position.Y -= 7.0f;
+                }
+        }
+
+        public Rectangle collideRect
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)position.X,
+                    (int)position.Y,
+                    texture.Width,
+                    texture.Height);
+            }
+        }
+    }
+}
---
+++ PongPrototype/PongPrototype/PongPrototype/Ball.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PongPrototype
+{
+    class Ball : AI
+    {
+        Vector2 speed = new Vector2(9, 3);
+
+        public TimeSpan ballStart = TimeSpan.FromSeconds(3);
+
+        public Ball(Texture2D texture, Vector2 position, string soundFile)
+            : base(texture, position, soundFile)
+        { }
+
+        bool CPUserve()
+        {
+                if(position.Y < 0)
+                {
+                    return true;
+                }
+            return false;
+        }
+
+        public void Update(GameTime gameTime, Rectangle clientBounds)
+        {
+            ballStart -= gameTime.ElapsedGameTime;
+            if (ballStart <= TimeSpan.Zero)
+            {
+                //if (CPUserve())
+                //{
+                    position += speed;
+                //}else {
+                //    position += speed;
+                //}
+
+                //if (position.X < 0)
+                //    speed.X *= -1;
+                if (position.Y < 0)
+                    speed.Y *= -1;
+                //if (position.X > clientBounds.Width - texture.Width)
+                //    speed.X *= -1;
+                if (position.Y > clientBounds.Height - texture.Height)
+                    speed.Y *= -1;
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 1);
+        }
+
+        public float reverseSpeed()
+        {
+            return speed.X *= -1f;
+        }
+
+        public string countDown()
+        {
+            if (ballStart <= new TimeSpan(0, 0, 3) && ballStart >= new TimeSpan(0, 0, 2))
+            {
+                return "Ready?";
+            }
+            if (ballStart <= new TimeSpan(0, 0, 2) && ballStart >= new TimeSpan(0, 0, 1))
+            {
+                return "Set.";
+            }
+            if (ballStart <= new TimeSpan(0, 0, 1) && ballStart >= new TimeSpan(0, 0, 0))
+            {
+                return "GO!";
+            }
+
+            return string.Empty;
+        }
+    }
+}
